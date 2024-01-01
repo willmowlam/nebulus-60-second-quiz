9cc5e390655091ce6e09f6e5ef701a91bd3e208b
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-01 20:49:38 +0000

    Add questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,1 +1,52 @@
-
+const questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "Arrays in JavaScript can be used to store ____.",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: "all of the above"
+  },
+  {
+    title: "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+    answer: "quotes"
+  },
+  {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "terminal / bash", "for loops", "console log"],
+    answer: "console log"
+  },
+  {
+    title: "What is the result of the following expression: 2 + '2'?",
+    choices: ["4", "22", "TypeError", "NaN"],
+    answer: "22"
+  },
+  {
+    title: "Which of the following is a correct way to declare a JavaScript variable?",
+    choices: ["let variableName;", "variable variableName;", "var variableName;", "const variableName;"],
+    answer: "var variableName;"
+  },
+  {
+    title: "What does the JavaScript `===` operator check for?",
+    choices: ["Equality in value and type", "Equality in value only", "Assignment", "Inequality"],
+    answer: "Equality in value and type"
+  },
+  {
+    title: "Which of the following is NOT a valid way to comment in JavaScript?",
+    choices: ["// This is a comment", "/* This is a comment */", "# This is a comment", "<!-- This is a comment -->"],
+    answer: "# This is a comment"
+  },
+  {
+    title: "What does the JavaScript `setTimeout` function do?",
+    choices: ["Runs a function after a specified delay", "Halts the execution of the script", "Sets the value of a variable", "Returns the current date and time"],
+    answer: "Runs a function after a specified delay"
+  },
+]
