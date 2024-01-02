7266c2543672cd7b79a86987ca1bca2683a06461
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-02 18:15:43 +0000

    Clear high scores button

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,1 +1,8 @@
+// Page objects
+const listHighScores = document.getElementById("highscores");
+const buttonClearScores = document.getElementById("clear");
 
+// Clear scores
+buttonClearScores.addEventListener("click", function(event) {
+  localStorage.removeItem("highScores");
+});
