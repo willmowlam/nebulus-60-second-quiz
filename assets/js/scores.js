9fa19079223202a418f75e981e01b4618ba9b7a4
Will Mowlam <13359696+willmowlam@users.noreply.github.com>
2024-01-02 18:27:09 +0000

    Display high scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -5,4 +5,21 @@ const buttonClearScores = document.getElementById("clear");
 // Clear scores
 buttonClearScores.addEventListener("click", function(event) {
   localStorage.removeItem("highScores");
+  displayHighScores();
 });
+
+function displayHighScores(){
+  
+  // Set high scores or empty array if none exist yet
+  let highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+  for (let i = 0; i < highScores.length; i++) {
+    const score = highScores[i];
+    const li = document.createElement("li");
+    li.textContent = highScores[i].initials + " - " + highScores[i].score;
+    listHighScores.appendChild(li);
+  }
+
+};
+
+displayHighScores();
